ff97778302ee370c1042d2efe178f24aad09d25a
wangkaiwd <1162486176@qq.com>
2021-12-03 00:00:41 +0800

    feat: try generate best simple bundle and think result

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,18 @@
 export default {
   input: 'lib/index.js',
-  output: {
-    file: 'dist/es.js',
-    format: 'es'
-  }
+  output: [
+    {
+      file: 'dist/bundle.es.js',
+      format: 'es'
+    },
+    {
+      file: 'dist/bundle.cjs.js',
+      format: 'cjs'
+    },
+    {
+      file: 'dist/bundle.iife.js',
+      name: 'bundle',
+      format: 'iife'
+    }
+  ]
 };

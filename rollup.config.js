d18dead517e49bc402984f469a45e025f6971081
wangkaiwd <1162486176@qq.com>
2021-12-04 12:09:02 +0800

    feat: build with babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,6 @@
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+
 export default {
   input: 'lib/index.js',
   output: [
@@ -14,5 +17,7 @@ export default {
       name: 'bundle',
       format: 'iife'
     }
-  ]
+  ],
+  // https://github.com/rollup/plugins/tree/master/packages/babel#babelhelpers
+  plugins: [resolve(), babel({ babelHelpers: 'bundled' })]
 };
